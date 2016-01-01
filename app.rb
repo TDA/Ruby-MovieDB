2fbf648990ce0dd00f3aa03bf5ba32cda7612d02
Sai Pc <schand31@asu.edu>
2016-01-01 22:51:21 +0530

    basic request variables etc

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
+require './lib/movie'
 
 get('/movies') do
   # call erb with the symbol corresponding to ./views/xyz.erb
   # ie :xyz for the above, so never call this from lib or other folders :D
+  request.env.each { |item, value| puts "#{item } : #{value }"}
   erb :index
 end
