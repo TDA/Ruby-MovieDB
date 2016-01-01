61f556b8b7b0710458167a9490620faebdf2834b
Sai Pc <schand31@asu.edu>
2016-01-01 23:02:24 +0530

    ERB and its cross usage from the ruby files, added comments

--- app.rb
+++ app.rb
@@ -5,9 +5,12 @@ get('/movies') do
   # call erb with the symbol corresponding to ./views/xyz.erb
   # ie :xyz for the above, so never call this from lib or other folders :D
   request.env.each { |item, value| puts "#{item } : #{value }"}
+  # by storing these as instance variables they will be
+  # available in the erb files as well
   @movie = Movie.new
   @movie.title = 'TDKR'
   @movie.director = 'Chris Nolan'
   @movie.year = '2008'
+  # cant seem to call multiple files, will need to figure out how
   erb :index
 end
