c777121e298bccfe515ce0b560bad02f0ffa1d63
Sai Pc <schand31@asu.edu>
2016-01-01 22:56:41 +0530

    ERB and its cross usage from the ruby files

--- app.rb
+++ app.rb
@@ -5,5 +5,9 @@ get('/movies') do
   # call erb with the symbol corresponding to ./views/xyz.erb
   # ie :xyz for the above, so never call this from lib or other folders :D
   request.env.each { |item, value| puts "#{item } : #{value }"}
+  @movie = Movie.new
+  @movie.title = 'TDKR'
+  @movie.director = 'Chris Nolan'
+  @movie.year = '2008'
   erb :index
 end
